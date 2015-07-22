53084d6bc58a0af0fe2d84f424d000c43c0a299f
librasama <akiomoi@163.com>
2015-07-23 01:04:19 +0800

    websocket的一个实现源码cp。server端抄完了...

--- cpWebsocket/lib/WebSocketServer.js
+++ cpWebsocket/lib/WebSocketServer.js
@@ -283,6 +283,149 @@ function handleHybiUpgrade(req, socket, upgradeHead, cb) {
 }
 
 function handleHixieUpgrade(req, socket, upgradeHead, cb) {
+    var errorHander = function () {
+        try{socket.destroy();} catch(e){}
+    };
+    socket.on('error', errorHander);
+
+    if(this.options.disableHixie) {
+        abortConnection(socket, 401, 'Hixie support disabled');
+        return;
+    }
+
+    if(req.headers['sec-websocket-key2']) {
+        abortConnection(socket, 401, 'Bad Request');
+        return;
+    }
+
+    var origin = req.headers['origin'], self = this;
+
+    //准备就绪了
+    var onClientVerify = function () {
+        var wshost;
+        if(!req.headers['x-forwarded-host']) {
+            wshost = req.headers.host;
+        } else {
+            wshost = req.headers['x-forwarded-host'];
+        }
+        //判断加密咩
+        var location = ((req.headers['x-forworded-proto'] === 'https' || socket.encrypted)?'wss':'ws'  + '://' + wshost+req.url),
+            protocol = req.headers['sec-websocket-protocol'];
+        // 完成握手
+        var completeHandshake = function (nonce, rest) {
+
+            // 计算值
+            var k1 = req.headers['sec-websocket-key1'],k2 = req.headers['sec-websocket-key2'], md5 = crypto.createHash('md5');
+            [k1,k2].forEach(function (k) {
+                var n = parseInt(k.replace(/[^\d]/g, '')),
+                   spaces = k.replace(/[^\d]/g, '').length; //牛B...为了抽出数字也蛮拼的
+                if(spaces === 0 || n% spaces !== 0) {
+                    abortConnection(socket, 400, 'Bad Request');
+                    return;
+                }
+                n /= spaces;
+                md5.update(String.fromCharCode(
+                    n >> 24 & 0xFF,
+                    n >> 16 & 0xFF,
+                    n >> 8  & 0xFF,
+                    n       & 0xFF
+                ));
+                md5.update(nonce.toString('binary'));
+                var headers = [
+                    'HTTP/1.1 101 Switching Protocols',
+                    'Upgrade: websocket',
+                    'Connection: upgrade',
+                    'Sec-Websocket-Location: ' + location
+                ];
+                if(typeof protocol != 'undefined') {
+                    headers.push('Sec-WebSocket-Protocol: '+protocol);
+                }
+                if(typeof origin != 'undefined') {
+                    headers.push('Sec-WebSocket-Origin: '+origin);
+                }
+                socket.setTime(0);
+                socket.setNoDelay(true);
+
+                try {
+                    // 合并头部和hash buffer
+                    var headBuffer = new Buffer(headers.concat('', '').join('\r\n'));
+                    var hashBuffer = new Buffer(md5.digest('binary'), 'binary');
+                    var handshakeBuffer = new Buffer(headBuffer.length + hashBuffer.length);
+                    headBuffer.copy(handshakeBuffer, 0);
+                    hashBuffer.copy(handshakeBuffer, headBuffer.length);
+
+                    // 单独写，当成功的时候会新建一个websocket client
+                    socket.write(handshakeBuffer, 'binary', function(err){
+                        if(err) return;
+                        var client = new WebSocket([req, socket, rest], {
+                            protocolVersion:'hixie-76',
+                            protocol:protocol
+                        });
+                        if(self.options.clientTracking) {
+                            self.clients.push(client);
+                            client.on('close', function () {
+                                var index = self.clients.indexOf(client);
+                                if(index != -1) {
+                                    self.clients.splice(index, 1);
+                                }
+                            });
+                        }
+                        socket.removeListener('error', errorHander);
+                        cb(client);
+                    });
+
+                } catch (e) {
+                    try{socket.destory();} catch(e){}
+                }
+
+            });
+        };
+        // 获取nonce？nonce是个什么鬼
+        var nonceLength = 0;
+        if(upgradeHead && upgradeHead.length >= nonceLength) {
+            var nonce = upgradeHead.slice(0, nonceLength);
+            var rest = upgradeHead.length > nonceLength ? upgradeHead.slice(nonceLength):null;
+            completeHandshake.call(self, nonce, rest);
+        } else {
+            // 获取到的head不足，所以必须等到足够的data才能继续~~（是足够长了就行吗？= =||）
+            var nonce = new Buffer(nonceLength);
+            upgradeHead.copy(nonce, 0);
+            var received = upgradeHead.length;
+            var handler = function (data) {
+                var toRead = Math.min(data.length, nonceLength - received);
+                if(toRead === 0) return;
+                data.copy(nonce, received, 0, toRead);
+                received += toRead;
+                if(received == nonceLength) {
+                    socket.removeListener('data', handler);
+                    if(toRead < data.length) rest = data.slice(toRead);
+                    completeHandshake(self, nonce, rest);
+                }
+            }
+
+        }
+
+    };
+
+    if(typeof this.options.verifyClient == 'function') {
+        var info = {origin:origin, secure:typeof req.connection.authorized != 'undefined' || typeof req.connection.encrypted != 'undefined', req:req};
+        if(this.options.verifyClient.length == 2) {
+            this.options.verifyClient(info, function (result, code, name) {
+                if(typeof code === 'undefined') code = 401;
+                if(typeof name === 'undefined') name = http.STATUS_CODES[code];
+
+                if(!result) abortConnection(socket, code, name);
+                else onClientVerify.apply(this);
+            });
+            return;
+        } else if(!this.options.verifyClient(info)) {
+            abortConnection(socket, 401, 'Unauthorized');
+            return;
+        }
+    }
+    onClientVerify();
+
+
 
 }
 
