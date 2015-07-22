3bb3b92412d4d8899054bf76a6df3f0a3f209fc6
librasama <akiomoi@163.com>
2015-07-22 19:39:20 +0800

    websocket little demo

---
+++ apisrc/mywebsocket.js
@@ -0,0 +1,13 @@
+var WebSocket = require('ws');
+var con = new WebSocket('wss://echo.websocket.org/');
+con.onopen = function () {
+    con.send('Wooo~~~');
+};
+
+con.onerror = function(err) {
+    console.log("Websocket error: " + JSON.stringify(err));
+};
+
+con.onmessage = function (e) {
+    console.log("Server:"+ e.data);
+};
